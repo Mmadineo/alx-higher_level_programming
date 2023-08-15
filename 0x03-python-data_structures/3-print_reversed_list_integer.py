457fc2ba39e475d0a6c3aa9489b7762b4e77ee01
Mmadineo <mmadineo.seleke@gmail.com>
2023-08-15 07:06:22 -0700

    Task 3

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    if isinstamce(my_list,list):
+        my_list_reverse()
+        for i in my_list:
+            print("{:d}".format(i))
