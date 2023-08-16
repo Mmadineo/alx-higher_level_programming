d635ac3d31f041d95638a8030c626ae1f299347f
Mmadineo <mmadineo.seleke@gmail.com>
2023-08-16 01:46:20 -0700

    Task 6 Correction

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from calculator_1 import add, sub, div, mul
+    from calculator_1 import add, sub, mul, div
     import sys
 
     if len(sys.argv) - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-        ops = {"+": add, "-": sub, "/": div, "*": mul}
-
+        ops = {"+": add, "-": sub, "*": mul, "/": div}
         if sys.argv[2] not in list(ops.keys()):
-            print("Unknown operator. Avalable operators: +, -, / and *")
+            print("Unknown operator. Avalable operators: +, -, * and /")
             sys.exit(1)
             a = int(sys.argv[1])
             b = int(sys.argv[3])
