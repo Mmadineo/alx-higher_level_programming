4b224dd5b75071433c79d3eb7f12e67a232e1656
Mmadineo <mmadineo.seleke@gmail.com>
2023-08-17 02:23:47 -0700

    Task 5 correction

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def number_keys(a_dictionary):
     num = 0
-    list_keys = list(a_dictionary)
-    for i in list_keys:
+    nb_keys = list(a_dictionary)
+    for i in nb_keys:
         num += 3
         return (num)
