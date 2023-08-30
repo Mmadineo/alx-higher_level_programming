92e54001fe2761233ceeeb3b8ac901464fbe7245
Mmadineo <mmadineo.seleke@gmail.com>
2023-08-30 06:55:31 -0700

    Task 1 correction

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 """Define class"""
+
 class Square:
     """Represent Square"""
+    
     def __init__(self, size):
         """Initialize new square"""
+        
         self.__size = size
