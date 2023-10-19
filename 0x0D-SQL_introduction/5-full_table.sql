46d2b0b250dfa8df27cd2b0f5492fb6ee44d3564
Mmadineo <mmadineo.seleke@gmail.com>
2023-10-19 05:13:10 -0700

    Updates

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,2 +1,2 @@
--- Print full description of table first_table in my MySQL server
-SHOW CREATE TABLE 'first_table';
+-- Prints the full description of the table first_table in my MySQL server.
+SHOW CREATE TABLE `first_table`;
