f801c9cbe7cb5529520775832be06fbe9107c316
Mmadineo <57712435+Mmadineo@users.noreply.github.com>
2024-01-15 05:52:58 +0200

    Delete 0x0E-SQL_more_queries directory

--- 0x0E-SQL_more_queries/3-force_name.sql
+++
@@ -1,3 +0,0 @@
--- Creates the table force_name
--- Creates a table in a database
-CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++
@@ -1,3 +0,0 @@
--- Lists all cities contained in database hbtn_0d_usa
--- Lists all rows of a particular column in a database
-SELECT cities.id, cities.name, states.name FROM cities LEFT JOIN states ON states.id = cities.state_id ORDER BY cities.id;
